d2099554e3c56073b95501959ade40e981227bec
Liz Dahlstrom <edahlstrom@pivotal.io>
2020-05-12 16:38:47 -0700

    upgrade to current react

---
+++ webpack-modules/src/App.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Observable } from 'rxjs';
+import { PetData } from "./Api";
+
+const App: React.FC = () => {
+
+    const [loading, setLoading] = useState<boolean>(true);
+    const [pets, setPets] = useState<PetData>();
+
+    const observeMessages = (sseUrl: string): Observable<string> => {
+        return new Observable<string>(obs => {
+            const es = new EventSource(sseUrl);
+            es.addEventListener('message', (evt) => {
+                console.log(evt.data);
+                obs.next(evt.data);
+            });
+            return () => es.close();
+        });
+    }
+    useEffect(() => {
+        const subscription = observeMessages('http://localhost:5555/stream')
+            .subscribe((data: string) => {
+                setLoading(true);
+                const parsedData: PetData = JSON.parse(data);
+                setPets(parsedData);
+                setLoading(false);
+            });
+        return () => subscription.unsubscribe();
+    }, []);
+
+    const showAnimal = (animal: string[]) => {
+        return animal.map(x => (<li>{x}</li>));
+    };
+
+    const showPetData = (petData: PetData) => {
+        return (
+            <div>
+                <div>Already adopted: {Math.round(petData.total_adopted)}</div>
+                <div>Cats still available</div>
+                <ul>
+                    {showAnimal(petData.cats)}
+                </ul>
+            </div>
+        );
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <div>{loading || !pets ? 'Loading...' : showPetData(pets)}</div>
+            </header>
+        </div>
+    );
+}
+
+export default App;
